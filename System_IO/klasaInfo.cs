b2070a27aaf284c76d75f1e2568606e161599c5a
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-04-05 13:11:26 +0200

    Add files via upload

---
+++ System_IO/klasaInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace System_IO
+{
+    internal class klasaInfo
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo (@"c:\");
+            Console.WriteLine("\n Poddirektoriji: ");
+
+            foreach (DirectoryInfo diInfo in di.GetDirectories())
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", diInfo.Name, diInfo.CreationTime, diInfo.LastAccessTime);
+            }
+
+            foreach (DirectoryInfo fiInfo in di.GetDirectories())
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", fiInfo.Name, fiInfo.CreationTime, fiInfo.LastAccessTime);
+            }
+
+        }
+    }
+}
---
+++ System_IO/klasaPath.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace System_IO
+{
+    internal class klasaPath
+    {
+
+        static void Main(string[] args)
+        {
+            string prviDirektorij = @"c:\prviDir";
+            string drugiDirektorij = @"c:\drugiDir";
+
+            string[] nazivi = new string[5];
+
+            if (!Directory.Exists(prviDirektorij))
+            {
+                Directory.CreateDirectory(prviDirektorij);
+            }
+
+            if (!Directory.Exists(drugiDirektorij))
+            {
+                Directory.CreateDirectory (drugiDirektorij);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                nazivi[i] = "Datoteka" + i;
+                if (!File.Exists(@"c:\prviDir\" + nazivi[i]))
+                {
+                    File.Create(@"c:\prviDir\" + nazivi[i]);
+                }
+
+            }
+            try
+            {
+                foreach (string datoteka in Directory.GetFiles(prviDirektorij))
+                {
+                    string imeDatoteke = Path.GetFileName(datoteka);
+                    string ciljnaDatoteka = Path.Combine(drugiDirektorij, imeDatoteke);
+
+                    File.Copy(datoteka, ciljnaDatoteka, false);
+                }
+                Console.WriteLine("It is DONE!");
+            }
+            catch(Exception greska)
+            {
+                Console.WriteLine("Greška: {0}", greska.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
